22e513a4e416f8b442110c1c7a4cefe975462cc2
chingwu99 <henry88028802@hotmail.com>
2022-09-28 19:26:08 +0800

    add services nav options

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ hambuger.addEventListener("click", (e) => {
     });
   });
 });
+
+//service-nav-open
+let serviceNav = document.querySelector(".service-nav");
+serviceNav.addEventListener("mouseover", (e) => {
+  let serviceNavOpen = document.querySelector(".service-nav-open");
+  serviceNavOpen.classList.add("service-nav-mouseover");
+  serviceNavOpen.style.animation = "slidedown 0.3s forwards";
+
+  let serviceNavMouseover = () => {
+    serviceNavOpen.classList.remove("service-nav-mouseover");
+  };
+
+  serviceNavOpen.addEventListener("mouseout", (e) => {
+    serviceNavMouseover();
+  });
+
+  document.querySelectorAll(".service-open-li").forEach((serviceOpenLi) => {
+    serviceOpenLi.addEventListener("click", (e) => {
+      document.querySelector(".service-nav-open");
+      serviceNavMouseover();
+    });
+  });
+
+  serviceNav.addEventListener("click", (e) => {
+    document.querySelector(".service-nav-open");
+    serviceNavMouseover();
+  });
+});

f1932f948d1057ee6b4a1ee70ac66eab0a4fb039
chingwu99 <henry88028802@hotmail.com>
2022-10-02 22:19:23 +0800

    add  Lighting Equipment Rental service

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//globle
 //hambuger-nav
 let hambuger = document.querySelector(".hambuger");
 hambuger.addEventListener("click", (e) => {
@@ -18,25 +19,94 @@ let serviceNav = document.querySelector(".service-nav");
 serviceNav.addEventListener("mouseover", (e) => {
   let serviceNavOpen = document.querySelector(".service-nav-open");
   serviceNavOpen.classList.add("service-nav-mouseover");
-  serviceNavOpen.style.animation = "slidedown 0.3s forwards";
+  serviceNavOpen.style.animation = "slidedown 0.5s forwards";
 
-  let serviceNavMouseover = () => {
+  let removeServiceNavMouseover = () => {
     serviceNavOpen.classList.remove("service-nav-mouseover");
   };
 
   serviceNavOpen.addEventListener("mouseout", (e) => {
-    serviceNavMouseover();
+    removeServiceNavMouseover();
   });
 
   document.querySelectorAll(".service-open-li").forEach((serviceOpenLi) => {
     serviceOpenLi.addEventListener("click", (e) => {
       document.querySelector(".service-nav-open");
-      serviceNavMouseover();
+      removeServiceNavMouseover();
     });
   });
 
   serviceNav.addEventListener("click", (e) => {
     document.querySelector(".service-nav-open");
-    serviceNavMouseover();
+    removeServiceNavMouseover();
   });
 });
+
+//lighting page
+//my equipment list
+let divList = document.querySelector(".list");
+let addList = document
+  .querySelectorAll(".commodity-button")
+  .forEach((commodityButton) => {
+    commodityButton.addEventListener("click", (e) => {
+      let commodityCard = e.target.parentElement.parentElement.parentElement;
+      let commodityWord = e.target.parentElement.parentElement;
+      let product = commodityCard.children[0].querySelector("img").src;
+      let description = commodityWord.children[0].children[0].innerText;
+      let price = commodityWord.children[0].children[1].children[0].innerText;
+
+      let myList = document.createElement("div");
+      myList.classList.add("my-list");
+      myList.innerHTML = `
+      <div class="list-equipment">
+        <div class="list-product">
+         <img src="${product}">
+        </div>
+        <div>
+         ${description}
+        </div>
+        <div class="quantity-button">
+          <button>+</button>
+          1
+          <button>-</button>
+        </div>
+        <div>
+         ＄<span class="price">${price}</span>/day
+        </div>
+        <div>
+         <i class="fa-solid fa-xmark remove-equipment"></i>
+        </div>
+      </div>
+      `;
+
+      divList.appendChild(myList);
+
+      let totalFunction = () => {
+        let totalPriceArray = [];
+        document.querySelectorAll(".price").forEach((item) => {
+          totalPriceArray.push(Number(item.innerText));
+        });
+        console.log(totalPriceArray);
+
+        let totalPrice = totalPriceArray.reduce((a, b) => a + b, 0);
+        console.log(totalPrice);
+
+        let total = document.querySelector(".total");
+        total.innerHTML = `Total:${totalPrice}`;
+      };
+
+      totalFunction();
+
+      document
+        .querySelectorAll(".remove-equipment")
+        .forEach((removeEquipment) => {
+          removeEquipment.addEventListener("click", (e) => {
+            let myList = e.target.parentElement.parentElement;
+
+            myList.remove();
+
+            totalFunction();
+          });
+        });
+    });
+  });
